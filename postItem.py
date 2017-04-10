49230b85259db0e7d04615953e63f185c3d74402
ehanson8 <ehanson8@jh.edu>
2017-04-10 15:13:20 -0400

    updates

--- postItem.py
+++ postItem.py
@@ -1,11 +1,14 @@
 import json
 import requests
 import secrets
+import datetime
 
 baseURL = secrets.baseURL
 email = secrets.email
 password = secrets.password
 filePath = secrets.filePath
+
+directory = filePath+raw_input('Enter directory name: ')
 
 data = json.dumps({'email':email,'password':password})
 header = {'content-type':'application/json','accept':'application/json'}
@@ -13,14 +16,13 @@ session = requests.post(baseURL+'/rest/login', headers=header, data=data).conten
 headerAuth = {'content-type':'application/json','accept':'application/json', 'rest-dspace-token':session}
 headerAuthFileUpload = {'accept':'application/json', 'rest-dspace-token':session}
 print 'authenticated'
-#
+
 #Post community
 communityName = 'Test Community'
 community = json.dumps({'name': communityName})
 post = requests.post(baseURL+'/rest/communities', headers=headerAuth, data=community).json()
 print json.dumps(post)
 communityID = post['link']
-print communityID
 
 #Post collection
 collectionName = 'Test Collection'
@@ -29,58 +31,44 @@ post = requests.post(baseURL+communityID+'/collections', headers=headerAuth, dat
 print json.dumps(post)
 collectionID = post['link']
 
-#Post item
-item = json.dumps({'metadata': [{'key': 'dc.title', 'language': 'en_US', 'value': 'testing123'}]})
-post = requests.post(baseURL+collectionID+'/items', headers=headerAuth, data=item).json()
-print json.dumps(post)
-itemID = post['link']
+#Post items
+collectionMetadata = json.load(open(filePath+'sampleCollectionMetadata.json'))
+for itemMetadata in collectionMetadata:
+    for element in itemMetadata['metadata']:
+        if element['key'] == 'dc.identifier.other':
+            imageIdentifier = element['value']
+    itemMetadata = json.dumps(itemMetadata)
+    post = requests.post(baseURL+collectionID+'/items', headers=headerAuth, data=itemMetadata).json()
+    print json.dumps(post)
+    itemID = post['link']
 
-#Post bitstream
-#bitstream = filePath+'test.txt'
-bitstream = filePath+'testImage.jpg'
-#bitstream = filePath+'A.pdf'
-#bitstream = filePath+'test.pdf'
+    #Post bitstream
+    bitstream = directory+'/'+imageIdentifier+'.jpg'
+    fileName = bitstream[bitstream.rfind('/')+1:]
+    data = open(bitstream, 'rb')
+    files = {'file': open(bitstream, 'rb')}
+    post = requests.post(baseURL+itemID+'/bitstreams?name='+fileName, headers=headerAuthFileUpload, data=data).json()
 
-fileName = bitstream[bitstream.rfind('/')+1:]
-files = {'file': open(bitstream, 'rb')}
-data = json.dumps({'name': fileName, 'sequenceId': 1})
-post = requests.post(baseURL+itemID+'/bitstreams?name='+fileName, headers=headerAuthFileUpload, files=files).json()
-print json.dumps(post)
-metadata = requests.get(baseURL+itemID+'/metadata', headers=headerAuth).json()
+    #Create provenance note
+    provNote = {}
+    provNote['key'] = 'dc.description.provenance'
+    provNote['language'] = 'en_US'
+    bitstreams = requests.get(baseURL+itemID+'/bitstreams', headers=headerAuth).json()
+    bitstreamCount = len(bitstreams)
+    utc= datetime.datetime.utcnow()
+    utcTime = utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+    provNoteValue = 'Made available in DSpace on '+utcTime+' (GMT). No. of bitstreams: '+str(bitstreamCount)
+    for bitstream in bitstreams:
+        fileName = bitstream['name']
+        size = str(bitstream['sizeBytes'])
+        checksum = bitstream['checkSum']['value']
+        algorithm = bitstream ['checkSum']['checkSumAlgorithm']
+        provNoteValue = provNoteValue+' '+fileName+': '+size+' bytes, checkSum: '+checksum+' ('+algorithm+')'
+    provNote['value'] = provNoteValue
+    provNote = json.dumps([provNote])
 
-updatedMetadata = []
-for metadatum in metadata:
-    if metadatum['key'] != 'dc.description.provenance':
-        print 'yay'
-        updatedMetadata.append(metadatum)
-    else:
-        value = metadatum['value']
-        time = value[value.index('DSpace on ')+10:value.index(' (GMT)')]
-        print time
-        print 'nay'
-        provNote = {}
-        provNote['key'] = 'dc.description.provenance'
-        provNote['language'] = 'en_US'
-        bitstreams = requests.get(baseURL+itemID+'/bitstreams', headers=headerAuth).json()
-        bitstreamCount = len(bitstreams)
-        provNoteValue = 'Made available in DSpace on '+time+' (GMT). No. of bitstreams: '+str(bitstreamCount)
-        for bitstream in bitstreams:
-            fileName = bitstream['name']
-            size = str(bitstream['sizeBytes'])
-            checksum = bitstream['checkSum']['value']
-            print checksum
-            algorithm = bitstream ['checkSum']['checkSumAlgorithm']
-            print algorithm
-            provNoteValue = provNoteValue+' '+fileName+': '+size+' bytes, checkSum: '+checksum+' ('+algorithm+')'
-        print provNoteValue
-        provNote['value'] = provNoteValue
-        print provNote
-        updatedMetadata.append(provNote)
-updatedMetadata = json.dumps(updatedMetadata)
-
-delete = requests.delete(baseURL+itemID+'/metadata', headers=headerAuth)
-print delete
-post = requests.put(baseURL+itemID+'/metadata', headers=headerAuth, data=updatedMetadata)
-print post
+    #Post provenance note
+    post = requests.put(baseURL+itemID+'/metadata', headers=headerAuth, data=provNote)
+    print post
 
 logout = requests.post(baseURL+'/rest/logout', headers=headerAuth)
