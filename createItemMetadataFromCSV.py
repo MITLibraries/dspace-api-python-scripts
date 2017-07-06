28ddd5d326d6f2547752574b7487fe7ec1d94d59
ehanson8 <ehanson8@jh.edu>
2017-07-06 14:33:38 -0400

    updates

--- createItemMetadataFromCSV.py
+++ createItemMetadataFromCSV.py
@@ -30,18 +30,21 @@ with open(filename) as csvfile:
     metadataGroup = []
     for row in reader:
         metadata = []
-        createMetadataElementCSV('dc.creator', 'creator', '')
-        createMetadataElementCSV('dc.date', 'date', '')
-        createMetadataElementCSV('dc.subject', 'decade', 'en_US')
-        createMetadataElementCSV('dc.description.abstract', 'description', 'en_US')
-        createMetadataElementCSV('dc.description', 'notes', 'en_US')
-        createMetadataElementCSV('dc.subject', 'photographType', 'en_US')
-        createMetadataElementCSV('dc.relation.ispartof', 'location', 'en_US')
-        createMetadataElementCSV('dc.identifier', 'identifier', '')
-        createMetadataElementCSV('dc.format.extent', 'size', '')
-        createMetadataElementCSV('dc.format.medium', 'medium', 'en_US')
-        createMetadataElementCSV('dc.title', 'title', 'en_US')
-        createMetadataElementCSV('dc.subject', 'subjectType', 'en_US')
+        createMetadataElementCSV('dc.creator', 'Creator', '')
+        createMetadataElementCSV('dc.date.issued', 'structuredDate', '')
+        createMetadataElementCSV('dc.title', 'fullTitle', 'en_US')
+        createMetadataElementCSV('dc.description.abstract', 'Description', 'en_US')
+        createMetadataElementCSV('dc.description', 'Notes:', 'en_US')
+        createMetadataElementCSV('dc.identifier.other', 'File name', 'en_US')
+        createMetadataElementDirect('dc.language.iso', 'en_US', 'en_US')
+        createMetadataElementDirect('dc.type', 'Text', 'en_US')
+        createMetadataElementDirect('dc.format.mimetype','application/pdf', 'en_US')
+        # createMetadataElementCSV('dc.relation.ispartof', 'location', 'en_US')
+        # createMetadataElementCSV('dc.identifier', 'identifier', '')
+        # createMetadataElementCSV('dc.format.extent', 'size', '')
+        # createMetadataElementCSV('dc.format.medium', 'medium', 'en_US')
+        # createMetadataElementCSV('dc.title', 'title', 'en_US')
+        # createMetadataElementCSV('dc.subject', 'subjectType', 'en_US')
 
         item = {'metadata': metadata}
         metadataGroup.append(item)
--- postCollection.py
+++ postCollection.py
@@ -2,6 +2,8 @@ import json
 import requests
 import secrets
 import datetime
+import time
+import os
 
 secretsVersion = raw_input('To edit production server, enter the name of the secrets file: ')
 if secretsVersion != '':
@@ -19,11 +21,12 @@ verify = secrets.verify
 
 requests.packages.urllib3.disable_warnings()
 
-directory = filePath+raw_input('Enter directory name: ')
+directory = raw_input('Enter directory name: ')
 fileExtension = '.'+raw_input('Enter file extension: ')
-communityName = raw_input('Enter community name: ')
-collectionName = raw_input('Enter collectionn name: ')
+communityHandle = raw_input('Enter community handle: ')
+collectionName = raw_input('Enter collection name: ')
 
+startTime = time.time()
 data = json.dumps({'email':email,'password':password})
 header = {'content-type':'application/json','accept':'application/json'}
 session = requests.post(baseURL+'/rest/login', headers=header, verify=verify, data=data).content
@@ -33,18 +36,29 @@ status = requests.get(baseURL+'/rest/status', headers=headerAuth, verify=verify)
 userFullName = status['fullname']
 print 'authenticated'
 
-#Post community
-community = json.dumps({'name': communityName})
-post = requests.post(baseURL+'/rest/communities', headers=headerAuth, verify=verify, data=community).json()
-communityID = post['link']
+#Get community ID
+endpoint = baseURL+'/rest/handle/'+communityHandle
+community = requests.get(endpoint, headers=headerAuth, verify=verify).json()
+communityID = str(community['id'])
 
 #Post collection
 collection = json.dumps({'name': collectionName})
-post = requests.post(baseURL+communityID+'/collections', headers=headerAuth, verify=verify, data=collection).json()
+post = requests.post(baseURL+'/rest/communities/'+communityID+'/collections', headers=headerAuth, verify=verify, data=collection).json()
 collectionID = post['link']
 
 #Post items
-collectionMetadata = json.load(open(directory+'/'+'collectionMetadata.json'))
+fileList = {}
+for root, dirs, files in os.walk(directory, topdown=True):
+    for file in files:
+        if file.endswith(fileExtension):
+            fileList[file.replace('.pdf','')] = os.path.join(root, file).replace('\\','/')
+            print file
+elapsedTime = time.time() - startTime
+m, s = divmod(elapsedTime, 60)
+h, m = divmod(m, 60)
+print 'File list creation time: ','%d:%02d:%02d' % (h, m, s)
+
+collectionMetadata = json.load(open(directory+'/'+'metadata.json'))
 for itemMetadata in collectionMetadata:
     for element in itemMetadata['metadata']:
         if element['key'] == 'dc.identifier.other':
@@ -55,11 +69,19 @@ for itemMetadata in collectionMetadata:
     itemID = post['link']
 
     #Post bitstream
-    bitstream = directory+'/'+fileIdentifier+fileExtension
+    bitstream = fileList[fileIdentifier]
     fileName = bitstream[bitstream.rfind('/')+1:]
     data = open(bitstream, 'rb')
     files = {'file': open(bitstream, 'rb')}
     post = requests.post(baseURL+itemID+'/bitstreams?name='+fileName, headers=headerAuthFileUpload, verify=verify, data=data).json()
+
+
+    # #Post bitstream
+    # bitstream = directory+'/'+fileIdentifier+fileExtension
+    # fileName = bitstream[bitstream.rfind('/')+1:]
+    # data = open(bitstream, 'rb')
+    # files = {'file': open(bitstream, 'rb')}
+    # post = requests.post(baseURL+itemID+'/bitstreams?name='+fileName, headers=headerAuthFileUpload, verify=verify, data=data).json()
 
     #Create provenance notes
     provNote = {}
@@ -97,3 +119,8 @@ for itemMetadata in collectionMetadata:
     print post
 
 logout = requests.post(baseURL+'/rest/logout', headers=headerAuth, verify=verify)
+
+elapsedTime = time.time() - startTime
+m, s = divmod(elapsedTime, 60)
+h, m = divmod(m, 60)
+print 'Total script run time: ','%d:%02d:%02d' % (h, m, s)
