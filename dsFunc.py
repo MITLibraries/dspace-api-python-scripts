d9d046b26a3316cee17bafb79733ac8fc96346e3
Eric Hanson <ehanson@mit.edu>
2019-05-20 11:30:32 -0400

    Update dsFunc.py

--- dsFunc.py
+++ dsFunc.py
@@ -1,5 +1,28 @@
 import datetime
 import time
+import requests
+
+
+def auth(email, password, baseURL, verify):
+    """Authenticate the user to the DSpace API."""
+    data = {'email': email, 'password': password}
+    header = {'content-type': 'application/json',
+              'accept': 'application/json'}
+    session = requests.post(baseURL + '/rest/login', headers=header,
+                            verify=verify,
+                            params=data).cookies['JSESSIONID']
+    cookies = {'JSESSIONID': session}
+    return(cookies, header)
+
+
+def authConfirm(cookies, baseURL, header, verify):
+    """Confirm user was successfully authenticated to the DSpace API."""
+    status = requests.get(baseURL + '/rest/status', headers=header,
+                          cookies=cookies, verify=verify).json()
+    uName = status['fullname']
+    authEmail = status['email']
+    print('authenticated', uName, authEmail)
+    return(uName, authEmail)
 
 
 def elapsedTime(startTime, label):
